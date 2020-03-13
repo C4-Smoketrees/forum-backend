fb5236989aa621c6c4f7cc99e7c1b4353a71de01
Anshuman Chhapolia <achhap.10.01@gmail.com>
2020-03-13 16:38:31 +0530

    Add initial server files
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const compression = require("compression")
+const sanitizer = require("express-sanitizer");
+const parser = require("body-parser");
+
+// Initialise server
+var app = express();
+
+// Middlewares
+app.use(sanitizer);
+app.use(parser);
+app.use(compression);
+
+// Get the assigned environment port
+let port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+    console.info(`running the forum server on ${port}`);
+});
