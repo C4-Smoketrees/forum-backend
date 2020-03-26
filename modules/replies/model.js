ab67458cc4a762e787294c47481cc4d9a8cccfa9
Anshuman Chhapolia <achhap.10.01@gmail.com>
2020-03-26 18:52:57 +0530

    Add new reply method in model
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

--- modules/replies/model.js
+++ modules/replies/model.js
@@ -1,11 +1,52 @@
+const bson = require('bson')
+const logger = require('../../logging/logger')
+
 class Reply {
   constructor (object) {
+    this._id = object._id
     this.content = object.content
     this.content = object.author
     this.upvotes = object.upvotes
     this.downvotes = object.downvotes
     this.dateTime = object.dateTime
     this.reports = object.reports
+  }
+
+  async newReply (threadId, threadCollection) {
+    let response
+    try {
+      const filter = {
+        _id: bson.ObjectID.createFromHexString(threadId)
+      }
+      this._id = bson.ObjectId.generate()
+      this.dateTime = Date.now()
+      const query = {
+        $push: {
+          replies: this
+        }
+      }
+      const res = await threadCollection.update(filter, query)
+      if (res.modifiedCount !== 1) {
+        response = {
+          status: false,
+          msg: `Unable to create a reply for thread:${threadId} replyId:${this._id}`,
+        }
+        logger.debug(response.msg)
+      } else {
+        response = {
+          status: true,
+          msg: 'success',
+          replyId: this._id.toHexString()
+        }
+        logger.debug(`New reply for thread:${threadId} replyId:${this._id}`)
+      }
+    } catch (e) {
+      response = {
+        status: false,
+        err: e
+      }
+    }
+    return response
   }
 }
 
