21c425f804944ee9c9f785107f8a4cd41f7e60e9
Anshuman Chhapolia <achhap.10.01@gmail.com>
2020-04-11 14:19:42 +0530

    Add Client stub example
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

--- client-stub/client.js
+++ client-stub/client.js
@@ -1,25 +1,8 @@
-const fetch = require('node-fetch')
-const agent = require('./agent')
-
-const url = 'https://localhost:8443'
+const draft = require('./draft')
 
 const ForumClient = function () {
 }
 
-ForumClient.prototype.newDraft = async function (content, title, tags, token) {
-  const res = await fetch(url + '/drafts/new', {
-    method: 'POST',
-    body: JSON.stringify({ draft: { content: content, tags: tags, title: title } }),
-    headers: { Authorization: token, 'Content-Type': 'application/json' },
-    agent
-  })
-  console.log(res.status)
-  console.log(res)
-  const response = await res.text()
-  console.log(response)
-  if (response.status) {
-    return response.draftId
-  }
-}
+ForumClient.prototype.newDraft = draft.newDraft
 
 module.exports = new ForumClient()
