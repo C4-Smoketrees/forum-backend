7ca1db3eaee360ae2b589091b1b250a474ba4c96
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-04-12 00:29:24 +0530

    Structure of other route stubs created but still incomplete

--- client-stub/draft.js
+++ client-stub/draft.js
@@ -13,5 +13,52 @@ async function newDraft (content, title, tags, token) {
   const body = await res.json()
   return body
 }
+async function getAllDraft(token){
+  const res=await fetch(url + '/drafts/all',{
+    method: 'GET',
+    body: , 
+    headers: {Authorization: token, 'Content-Type': 'application/json'},
+    agent
+  })
 
+  const body= await res.json()
+  return body
+}
+
+async function getOneDraft(token){
+  const res=await fetch(url + '/drafts/one',{
+    method: 'GET',
+    body: ,
+    headers: {Authorization: token, 'Content-Type': 'application/json'},
+    agent
+  })
+  const body=await res.json()
+  return body
+}
+
+async function publishDraft(token)
+{
+  const res=await fetch(url + '/drafts/publish',{
+    method: 'POST',
+    body: ,
+    headers: {Authorization: token, 'Content-Type': 'application/json'},
+    agent
+  })
+
+  const body=await res.json()
+  return body
+}
+
+async function deleteDraft(token)
+{
+  const res=await fetch(url + '/drafts/delete',{
+    method: 'POST',
+    body: ,
+    headers: {Authorization: token, 'Content-Type': 'application/json'},
+    agent
+  })
+
+  const body=await res.json();
+  return body
+}
 exports.newDraft = newDraft
